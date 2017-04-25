b2e21b440e9022272aba74e7e2633e97eac95bae
Socha17 <mattsocha11@gmail.com>
2017-04-25 18:31:55 +0000

    now returning info based on argv taken

--- test_script.js
+++ test_script.js
@@ -1,3 +1,5 @@
+
+
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
@@ -11,14 +13,21 @@ const client = new pg.Client({
 });
 
 client.connect((err) => {
+  let someName = process.argv[2];
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT * famous_people", (err, result) => {
+
+  client.query('SELECT * FROM famous_people WHERE first_name LIKE $1::text OR last_name LIKE $1::text;', [`${someName}`], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0].number); //output: 1
+    console.log("Searching...");
+    console.log(`Found result by the name of ${someName}`);
+
+  console.log(`- ${result.rows[0].id}  ${result.rows[0].first_name} born  ${result.rows[0].birthdate.getFullYear()}-${result.rows[0].birthdate.getDay()}-${result.rows[0].birthdate.getMonth()}`);
+
+    // console.log(result.row); //output: 1
     client.end();
   });
 });
